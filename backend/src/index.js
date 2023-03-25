e609a56f61d66f67c0dba54108e9db104df94d7c
gowtham <gowtham2004123@gmail.com>
2023-03-25 12:19:56 +0530

    mysql connection with node

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,7 @@
 import express from 'express';
 import cors from "cors"
-
-const app = express()
-const port = 3001;
-
+import mysql from "mysql";
+const app = express();
 app.use(express.json())
 
 app.use(
@@ -19,11 +17,34 @@ app.get("/", (req, res) => {
 
     return res.send(JSON.stringify(msg))
 })
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '12345',
+    database: 'hotel_management',
+  });
+  
+  db.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL:', err);
+      return;
+    }
+    console.log('Connected to MySQL database!');
+  });
+  
+  app.get('/users', (req, res) => {
+    const sql = 'SELECT * FROM demo';
+    db.query(sql, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      res.send(result);
+    });
+  });
 
-
-
+  const port = 3001;
 app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
 
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react' 
 import './App.css'
 
-
 function App() {
   const url = "http://localhost:3001/"
 
